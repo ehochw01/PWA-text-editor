bc6562f51aa41fad1dddcf0af746efde953624df
Eric Hochwald <erichochwald@erics-mbp.myfiosgateway.com>
2022-07-19 23:35:07 -0400

    tweaked database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,13 +13,13 @@ const initdb = async () =>
   });
 
 // Adds logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => {
+export const putDb = async (id, content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
-  const store = tx.objectStore('jate');
-  const request = store.put({ jate: content });
+  const store = tx. objectStore('jate');
+  const request = store.put({ id: id, data: content });
   const result = await request;
-  console.log('🚀 - data saved to the database', result);
+  console.log('data saved successfuly.', result);
 };
 
 // Adds logic for a method that gets all the content from the database
