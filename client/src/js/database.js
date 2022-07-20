b4fb2575db814b5c60d209f129dc8a760c32aff3
Eric Hochwald <erichochwald@erics-mbp.myfiosgateway.com>
2022-07-20 10:05:41 -0400

    fixed database bug

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,11 +13,11 @@ const initdb = async () =>
   });
 
 // Adds logic to a method that accepts some content and adds it to the database
-export const putDb = async (id, content) => {
+export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
-  const store = tx. objectStore('jate');
-  const request = store.put({ id: id, data: content });
+  const store = tx.objectStore('jate');
+  const request = store.put({id: 1, data: content });
   const result = await request;
   console.log('data saved successfuly.', result);
 };
@@ -27,7 +27,7 @@ export const getDb = async () => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const request = store.getAll();
+  const request = store.get(1);
   const result = await request;
   console.log('result.value', result);
   return result;
